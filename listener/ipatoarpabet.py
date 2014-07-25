117f04a4ea20b2a9d10dc4f482c6b1d40c90cb94
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-07-25 10:45:30 -0400

    Try to get the ipa => arpabet working... but wow it's not working

--- listener/ipatoarpabet.py
+++ listener/ipatoarpabet.py
@@ -11,38 +11,78 @@ def get_mapping( ):
     if MAPPING is None:
         MAPPING = {}
         for line in csv.reader( open( MAPPING_FILE, 'rb' ) ):
-            print line
-            MAPPING.setdefault(line[1],[]).append( line[0])
+            MAPPING.setdefault(line[1].decode('utf-8'),[]).append( line[0].decode('utf-8'))
     return MAPPING
 
 def get_espeak( word ):
     return subprocess.check_output([
         'espeak',
-        '--punct','-q','-x','--ipa=3',
+        '-q','--ipa=3',
         word,
-    ])
+    ]).decode('utf-8')
 def translate( ipa ):
     result = []
     mapping = get_mapping()
-    ipa = ipa.strip().replace('ˌ','').replace('ˈ','').split('_')
-    return ' '.join( [mapping.get(c,[c])[0] for c in ipa])
+    ipa = [
+        c.strip() for c in 
+        ipa.strip().replace(u' ',u'_').replace(
+            u'ˌ',u''
+        ).replace(
+            u'ˈ',u''
+        ).replace(
+            u'ː', u''
+        ).split(u'_')
+    ]
+    results = [ ]
+    for sound in ipa:
+        if sound in mapping:
+            sound = [sound]
+        for character in sound:
+            if not character:
+                continue
+            translations = mapping.get(character)
+            if not translations:
+                raise ValueError( character, ipa )
+            elif not results:
+                results = translations[:]
+            else:
+                results = [ r+' '+t for r in results for t in translations]
+    return results
 
 def test():
+    mapping = get_mapping()
+    assert u'ɛ' in mapping, mapping.keys()
     dictionary = os.path.expanduser( '~/.config/listener/default/lm/dictionary.dict' )
-    for line in open(dictionary):
-        try:
-            word,description = line.strip().split('\t',1)
-        except ValueError as err:
-            raise ValueError( line )
-        if word.endswith( ')' ):
-            word = word.rsplit('(',1)[0]
-        ipa = get_espeak( word )
-        try:
-            translated = translate( ipa )
-        except ValueError as err:
-            print( 'Failure on %r %s %s'%( word, ipa, err.args[0] ))
-            break
-        print( '%(word)s %(ipa)s %(translated)s'%locals())
+    good = bad = 0
+    try:
+        for line in open(dictionary):
+            try:
+                word,description = line.strip().split('\t',1)
+            except ValueError as err:
+                err.args += (line,)
+                raise
+            if word.endswith( ')' ):
+                word = word.rsplit('(',1)[0]
+            while word and not word[0].isalnum():
+                word = word[1:]
+            ipa = get_espeak( word )
+            try:
+                translated = translate( ipa )
+            except ValueError as err:
+                err.args += (word,ipa)
+                print( 
+                    u'Failed to translate: %s %s -> %s  char=%s(%r)'%(
+                    word,
+                    ipa, description, err.args[0], err.args[0],
+                ))
+                raise
+            if description in translated:
+                good += 1
+            else:
+                bad += 1
+                print( '%(word)s %(ipa)s\n\t%(translated)s\n\t%(description)s'%locals())
+    finally:
+        print '%s good %s bad %s'%(good,bad, (good/float(good+bad or 1)))
 
 if __name__ == '__main__':
     test()
