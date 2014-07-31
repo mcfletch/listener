7225e785db24cf9fee99bc8947b20de00a00351b
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-07-31 14:41:13 -0400

    Rework for reusability and understandability

--- listener/uinputdriver.py
+++ listener/uinputdriver.py
@@ -54,132 +54,141 @@ UINPUT_LOCATIONS = [
     '/dev/input/uinput',
 ]
 
-def our_device( ):
-    return uinput_user_dev(
-        name = 'listener voice keyboard',
-        id = input_id(
-            bustype=BUS_VIRTUAL,
-            vendor=1,
-            product=1,
-            version=1,
-        ),
-    )
+class UInput( object ):
+    def __init__( self ):
+        self.open_fd()
+        # Yes, this sucks, but it takes a while to be recognized
+        time.sleep( .01 )
+
+    @property
+    def our_device( self ):
+        return uinput_user_dev(
+            name = 'listener voice keyboard',
+            id = input_id(
+                bustype=BUS_VIRTUAL,
+                vendor=1,
+                product=1,
+                version=1,
+            ),
+        )
     
+    @property
+    def uinput_device(self):
+        for fn in UINPUT_LOCATIONS:
+            if os.path.exists( fn ):
+                return fn 
+        raise RuntimeError( "Did not find uinput device" )
 
-def uinput_device():
-    for fn in UINPUT_LOCATIONS:
-        if os.path.exists( fn ):
-            return fn 
-    raise RuntimeError( "Did not find uinput device" )
+    def write_bytes( self, bytes ):
+        original = bytes
+        while bytes:
+            written = os.write(self.fd, bytes )
+            if written == 0:
+                raise RuntimeError( 'Unable to write to input device?' )
+            else:
+                bytes = bytes[written:]
+        return original
 
-def write_bytes( fd, bytes ):
-    original = bytes
-    while bytes:
-        written = os.write(fd, bytes )
-        if written == 0:
-            raise RuntimeError( 'Unable to write to input device?' )
+    def open_fd( self, filename=None ):
+        filename = filename or self.uinput_device
+        self.fd = os.open( filename, os.O_WRONLY|os.O_NONBLOCK )
+        for typ in (EV_KEY,EV_SYN):
+            if fcntl.ioctl(self.fd, UI_SET_EVBIT, typ) < 0:
+                raise RuntimeError( 'Unable to set event bit %s'%(typ,) )
+        for i in range(256):
+            if fcntl.ioctl(self.fd, UI_SET_KEYBIT, i) < 0:
+                raise RuntimeError( 'Unable to set key code %s bit', i )
+        device = self.our_device
+        as_string = ctypes.string_at( ctypes.addressof(device),ctypes.sizeof(device) )
+        self.write_bytes( as_string )
+        if fcntl.ioctl( self.fd, UI_DEV_CREATE ) < 0:
+            raise RuntimeError( 'Unable to create virtual device' )
+
+    def _send_event( self, type=EV_KEY, code=65, value=1 ):
+        event = input_event(
+            type=type,
+            code=code,
+            value=value,
+        )
+        as_string = ctypes.string_at( ctypes.addressof(event),ctypes.sizeof(event))
+        self.write_bytes( as_string )
+
+    @contextlib.contextmanager
+    def key_pressed( self, code ):
+        if isinstance( code, (str,unicode)):
+            code = self.get_key_mapping()[code]
+        self._send_event( code=code, value=1 )
+        yield 
+        self._send_event( code=code, value=0 )
+    
+    def send_keypress( self, key='a'):
+        mapping = self.get_key_mapping()
+        uc = key.upper()
+        if uc in mapping:
+            code = mapping[uc]
         else:
-            bytes = bytes[written:]
-    return original
-
-def open_fd( filename=None ):
-    filename = filename or uinput_device()
-    fd = os.open( filename, os.O_WRONLY|os.O_NONBLOCK )
-    for typ in (EV_KEY,EV_SYN):
-        if fcntl.ioctl(fd, UI_SET_EVBIT, typ) < 0:
-            raise RuntimeError( 'Unable to set event bit %s'%(typ,) )
-    for i in range(256):
-        if fcntl.ioctl(fd, UI_SET_KEYBIT, i) < 0:
-            raise RuntimeError( 'Unable to set key code %s bit', i )
-    device = our_device()
-    as_string = ctypes.string_at( ctypes.addressof(device),ctypes.sizeof(device) )
-    write_bytes( fd, as_string )
-    if fcntl.ioctl( fd, UI_DEV_CREATE ) < 0:
-        raise RuntimeError( 'Unable to create virtual device' )
-    return fd
-
-def _send_event( fd, type=EV_KEY, code=65, value=1 ):
-    event = input_event(
-        type=type,
-        code=code,
-        value=value,
-    )
-    as_string = ctypes.string_at( ctypes.addressof(event),ctypes.sizeof(event))
-    write_bytes( fd, as_string )
-
-@contextlib.contextmanager
-def key_pressed( fd, code ):
-    _send_event( fd, code=code, value=1 )
-    yield 
-    _send_event( fd, code=code, value=0 )
-    
-def send_keypress( fd, key='a'):
-    mapping = get_key_mapping()
-    uc = key.upper()
-    if uc in mapping:
-        code = mapping[uc]
-    else:
-        log.warn( 'Could not find mapping for key %r', key )
-        return
-    if not key.islower():
-        # we want to be able to pass explicit command codes as uppercase too
-        with key_pressed(fd,code=mapping['RIGHTSHIFT']):
-            with key_pressed(fd,code=code):
+            log.warn( 'Could not find mapping for key %r', key )
+            return
+        if not key.islower():
+            # we want to be able to pass explicit command codes as uppercase too
+            with self.key_pressed(code=mapping['RIGHTSHIFT']):
+                with self.key_pressed(code=code):
+                    log.debug( 'Sending key %r(%r)', key, code )
+                    pass
+        else:
+            with self.key_pressed(code=code):
                 log.debug( 'Sending key %r(%r)', key, code )
                 pass
-    else:
-        with key_pressed(fd,code=code):
-            log.debug( 'Sending key %r(%r)', key, code )
-            pass
+    
+    def sync(self):
+        self._send_event( type=EV_SYN, code=SYN_REPORT,value=0)
+    
+    KEY_MAPPING = None
+    @classmethod
+    def get_key_mapping(cls,force_rescan=False):
+        if cls.KEY_MAPPING is None:
+            if (not force_rescan) and os.path.exists( KEY_MAPPING_FILE ):
+                cls.KEY_MAPPING = json.loads( open(KEY_MAPPING_FILE).read())
+            else:
+                lines = open( '/usr/include/linux/input.h' ).readlines()
+                mapping = {}
+                for key,value in [
+                    line[8:].strip().split()[:2] 
+                    for line in lines if line.startswith( '#define KEY_' )
+                ]:
+                    try:
+                        value = int(value)
+                    except ValueError as err:
+                        pass 
+                    else:
+                        mapping[key[4:]] = value 
+                mapping[' '] = mapping['SPACE']
+                cls.KEY_MAPPING = mapping
+        return cls.KEY_MAPPING
+    def close( self ):
+        if fcntl.ioctl(self.fd, UI_DEV_DESTROY) < 0:
+            raise RuntimeError( 'Unable to cleanly shut down device' )
         
-def sync(fd):
-    _send_event( fd, type=EV_SYN, code=SYN_REPORT,value=0)
-        
+    
 def main():
     logging.basicConfig( level=logging.INFO )
-    fd = open_fd( )
-    # Sigh, there's a setup/negotiation time before the driver is 
-    # able to send events... but there doesn't seem to be a way to 
-    # get a notification when it is over...
-    time.sleep( .01 )
-    mapping = get_key_mapping()
-    with key_pressed( fd, code=mapping['LEFTALT']):
-        with key_pressed( fd, code=mapping['TAB'] ):
+    uinput = UInput()
+    with uinput.key_pressed( 'LEFTALT' ):
+        with uinput.key_pressed( 'TAB' ):
             log.info( 'Switching to another window' )
-    sync(fd)
+    uinput.sync()
+    # sigh, again, takes a while to switch and the input system 
+    # doesn't take into account that alt-tab was intended to switch 
+    # focus to a new window :( 
     time.sleep( .1 )
     try:
         for char in 'hello world':
-            send_keypress(fd,char)
+            uinput.send_keypress(char)
     finally:
-        if fcntl.ioctl(fd, UI_DEV_DESTROY) < 0:
-            raise RuntimeError( 'Unable to cleanly shut down device' )
-
-KEY_MAPPING = None
-def get_key_mapping(force_rescan=False):
-    global KEY_MAPPING
-    if KEY_MAPPING is None:
-        if (not force_rescan) and os.path.exists( KEY_MAPPING_FILE ):
-            KEY_MAPPING = json.loads( open(KEY_MAPPING_FILE).read())
-        else:
-            lines = open( '/usr/include/linux/input.h' ).readlines()
-            mapping = {}
-            for key,value in [
-                line[8:].strip().split()[:2] 
-                for line in lines if line.startswith( '#define KEY_' )
-            ]:
-                try:
-                    value = int(value)
-                except ValueError as err:
-                    pass 
-                else:
-                    mapping[key[4:]] = value 
-            mapping[' '] = mapping['SPACE']
-            KEY_MAPPING = mapping
-    return KEY_MAPPING
+        uinput.close()
 
 def rebuild_mapping():
-    mapping = get_key_mapping(force_rescan=True)
+    mapping = UInput.get_key_mapping(force_rescan=True)
     content = json.dumps( mapping )
     with open( KEY_MAPPING_FILE,'w' ) as fh:
         fh.write( content )
