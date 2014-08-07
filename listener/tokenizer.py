291a25ed302f94f54aea6eb249deadc223e3cb86
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-08-07 12:40:59 -0400

    Just checkpointing to avoid work-loss

--- listener/tokenizer.py
+++ listener/tokenizer.py
@@ -1,7 +1,9 @@
-import unicodedata
+import unicodedata,logging,re
 from ._bytes import as_unicode
-from . import codetowords
+log = logging.getLogger(__name__)
 
+
+CODING = re.compile( r'coding[:=]\s*([-\w.]+)' )
 
 class Tokenizer( object ):
     def __init__( self,dictionary ):
@@ -57,6 +59,7 @@ class Tokenizer( object ):
         """Split runs of categories into individual tokens"""
         current_token = []
         for (category,chars) in runs_of_categories:
+            assert isinstance(chars,unicode), (type(chars),chars)
             if category in self.SEPARATES_WORDS:
                 if current_token:
                     yield current_token
@@ -67,29 +70,45 @@ class Tokenizer( object ):
         if current_token:
             yield current_token
     
-    HAS_LETTERS = set(['L','Lu','Ll','Lt','Lm','Lo'])
     def expand( self, runs_of_tokens ):
         """Dispatch to our processing functions to expand each token"""
+        if isinstance( runs_of_tokens, (bytes,unicode)):
+            runs_of_tokens = self.runs_of_tokens(
+                self.runs_of_categories( runs_of_tokens )
+            )
         # expand any embedded numbers first...
         for token in runs_of_tokens:
+            assert isinstance( token, list ), token
+            assert len(token),token
+            assert len(token[0]) == 2,token
             yield self.expand_token( token )
+    DUNDER = ('Pc','__')
     def expand_token( self, token ):
         result = []
-        current = u''
+        current = []
         def add_current():
             if current:
-                result.extend(codetowords.break_down_name( current ))
-        for (category,chars) in token:
-            if category == 'N':
-                add_current()
-                current = u''
-                result.extend( self.expand_N( [(category,chars)]))
-            elif category.startswith('P'):
-                add_current()
-                current = u''
-                result.extend( self.expand_P( [(category,chars)]))
-            else:
-                current += chars 
+                result.extend(self.parse_camel( current ))
+        # special cases...
+        try:
+            for (category,chars) in token:
+                if category == 'N':
+                    add_current()
+                    current = []
+                    result.extend( self.expand_N( [(category,chars)]))
+                elif category == 'Pc':
+                    add_current()
+                    current = []
+                    result.extend( self.expand_P( [(category,chars)]))
+                elif category.startswith('P'):
+                    add_current()
+                    current = []
+                    result.extend( self.expand_P( [(category,chars)]))
+                else:
+                    current.append( (category, chars) )
+        except (ValueError,TypeError) as err:
+            err.args += (token,)
+            raise
         add_current()
         return result 
     
@@ -106,11 +125,14 @@ class Tokenizer( object ):
         return result
     def expand_P( self, token ):
         combined = u''.join([x[1] for x in token])
+        fragments = [m.group(0) for m in self.PUNCT_ITER.finditer( combined )]
         result = []
-        for char in combined:
-            explicit = self.PUNCTUATION_NAMES.get(char)
+        for fragment in fragments:
+            explicit = self.PUNCTUATION_NAMES.get(fragment)
             if not explicit:
-                explicit = char+unicodedata.name(char).replace(u' ',u'-').lower()
+                explicit = fragment+unicodedata.name(
+                    fragment
+                ).replace(u' ',u'-').lower()
             result.append( explicit )
         return result
     DIGITS = {
@@ -167,7 +189,6 @@ class Tokenizer( object ):
         '...': '...ellipsis',
         '/': '/slash',
         ':': ':colon',
-        '://': ':colon /slash /slash',
         ';': ';semi-colon',
         '<': '<less-than',
         '=': '=equals',
@@ -180,58 +201,129 @@ class Tokenizer( object ):
         ']': ']close-bracket',
         '^': '^caret',
         '_': '_under-score',
+        '__': '__dunder',
         '`': '`back-tick',
         '{': '{open-brace',
         '|': '|bar',
         '}': '}close-brace',
         '~': '~tilde',
     }
+    LONG_PUNCT = sorted(
+        PUNCTUATION_NAMES.keys(),
+        key = lambda x: (len(x),x),
+        reverse=True,
+    )
+    PUNCT_ITER = re.compile( u'%s|.'%(
+        u'|'.join([
+            u'(%s)'%( 
+                re.escape(punct)
+            )
+            for punct in LONG_PUNCT
+        ])
+    ), re.U|re.M|re.I)
+
+    def __call__( self, text ):
+        """Iterate producing all expanded tokens for a given text"""
+        for statement in text:
+            tokens = []
+            for expanded in self.expand(statement):
+                tokens.extend( expanded )
+            yield tokens 
         
+    def parse_run_together( self, name ):
+        """Parse dictionary words that are run together"""
+        if not self.dictionary:
+            return [name]
+        # split up the name looking for runtogether words...
+        name = name.lower()
+        if name in self.dictionary:
+            return [name]
+        # TODO: use statistics to decide which sub-words are the most 
+        # *likely* to occur, rather than always searching for a longer match...
+        
+        # anything smaller than 1 is *always* in the dictionary...
+        prefixes = [name[:i] for i in range(1,len(name))]
+        mapped = self.dictionary.have_words( *prefixes )
+        possibles = []
+        for (prefix,translations) in sorted(mapped.items(),key=lambda x: len(x[0]),reverse=True):
+            if translations:
+                suffix = name[len(prefix):]
+                if suffix in self.dictionary:
+                    return [prefix,suffix]
+                remaining = self.parse_run_together( suffix )
+                if len(prefix) > 1 and remaining != [suffix]:
+                    return [prefix]+remaining
+                possibles.append( [ prefix ] + remaining )
+        suffixes = [name[-i:] for i in range(1,len(name))]
+        mapped = self.dictionary.have_words( *suffixes )
+        possibles = []
+        for (suffix,translations) in sorted(mapped.items(),key=lambda x: len(x[0]),reverse=True):
+            if translations:
+                prefix = name[:-len(suffix)]
+                if prefix in self.dictionary:
+                    return [prefix,suffix]
+                remaining = self.parse_run_together( prefix )
+                if len(suffix) > 1 and remaining != [prefix]:
+                    return remaining+[suffix]
+                possibles.append( remaining+[suffix] )
+        if len(name) < 3:
+            return [c for c in name]
+        return [name]
 
-def test_tokenizer_categories( ):
-    tok = Tokenizer(None)
-    for source,expected in [
-        ('ThisIsThat',['T','his','I','s','T','hat']),
-        ('this 23skeedoo',[u'this', u' ', u'23', u'skeedoo']),
-        ('Just so.',[u'J', u'ust', u' ', u'so', u'.']),
-        ('x != this',[u'x', u' ', u'!=', u' ', u'this']),
-        ('x == this',[u'x', u' ', u'==', u' ', u'this']),
-        (
-            'http://test.this.that/there?hello&ex#anchor',
-            [
-                u'http', u'://', u'test', u'.', u'this', u'.', u'that', 
-                u'/', u'there', u'?', u'hello', u'&', u'ex', u'#', u'anchor'
-            ]
-        ),
-        ('# What he said',[u'#', u' ', u'W', u'hat', u' ', u'he', u' ', u'said']),
-    ]:
-        raw_result = list(tok.runs_of_categories(source))
-        result = [x[1] for x in raw_result]
-        assert result == expected, (source,result,raw_result)
+    def parse_run_together_with_markup( self, name ):
+        base = self.parse_run_together( name )
+        if len(base) > 1:
+            return ['no-space']+base+['spaces']
+        return base
+        
+    def is_title( self, first, second ):
+        return first[0] == 'Lu' and len(first) == 1 and second[0] == "Ll"
+    def is_all_caps( self, name ):
+        return all([item[0]=='Lu' for item in name])
+    def is_camel( self, name ):
+        """Go through name checking for camel-case"""
+        while len(name)>1:
+            if not self.is_title( name[0], name[1] ):
+                return False 
+            name = name[2:]
+        return not name # no trailing bits...
 
-def test_tokenizer_tokens( ):
-    tok = Tokenizer(None)
-    for source,expected in [
-        ('ThisIsThat',[['T','his','I','s','T','hat']]),
-        ('This is that',[[u'T', u'his'], [u' '], [u'is'], [u' '], [u'that']]),
-        ('x != this',[[u'x'], [u' '], [u'!='], [u' '], [u'this']]),
-        ('0x3faD',[['0','x','3','fa','D']]),
-        ('!@#$%^&*()_+-=[]{}\\|:;\'",.<>/?',[[u'!@#$%^&*()'], [u'_'], [u'+-=[]{}\\|:;\'",.<>/?']]),
-        ('elif moo:\n\tthat()',[[u'elif'], [u' '], [u'moo'], [u':'], [u'\n\t'], [u'that'], [u'()']]),
-    ]:
-        raw_result = list(tok.runs_of_tokens( tok.runs_of_categories(source)))
-        result = [[x[1] for x in result] for result in raw_result]
-        assert result == expected, (source,result,raw_result)
+    def parse_camel( self, name ):
+        if isinstance( name, (bytes,unicode)):
+            name = list(self.runs_of_categories( name ))
+        
+        all_caps = self.is_all_caps( name )
+        cap_camel_case = self.is_camel( name )
+        camel_case = len(name) > 1 and self.is_camel(name[1:])
+        
+        
+        words = [x for x in split if not x.isdigit()]
+        split_expanded = []
+        for item in split:
+            if item.isdigit():
+                split_expanded.extend( [digit(x) for x in item])
+            else:
+                split_expanded.append( item )
 
-def test_expand( ):
-    tok = Tokenizer(None)
-    for source,expected in [
-        #('0x23ad',['zero','x','two','three','ad']), # word "Ad" as in advertisement
-        ('!==',['!exclamation-point', '=equals', '=equals']),
-        ('"',['"double-quote']),
-        ('ThisIsThat',['cap', 'camel', u'This', u'Is', u'That']),
-        ('23skido',['two', 'three', u'skido']),
-    ]:
-        raw_result = list(tok.runs_of_tokens( tok.runs_of_categories(source)))
-        expanded = list(tok.expand( raw_result ))[0]
-        assert expanded == expected, (source,expanded)
+        run_together_expansion = sum([
+            self.parse_run_together_with_markup(x) 
+            for x in split_expanded
+        ],[])
+        result = run_together_expansion
+        if len(words) == 0:
+            # e.g. numeric fragment of a name...
+            pass
+        elif len(words) == 1:
+            if words[0].isupper():
+                result = ['all','caps'] + run_together_expansion
+            elif words[0].title() == words[0]:
+                result = ['cap']+run_together_expansion
+        else:
+            if all_caps:
+                result = ['all', 'caps'] + run_together_expansion
+            elif cap_camel_case:
+                result = ['cap','camel'] + run_together_expansion
+            elif camel_case and len(split) > 1:
+                result = ['camel'] + run_together_expansion
+        return result
+
---
+++ tests/test_tokenizer.py
@@ -0,0 +1,92 @@
+from unittest import TestCase
+import tempfile, shutil, os, time
+from listener import tokenizer, context
+HERE = os.path.dirname( __file__ )
+
+class TokenizerTests( TestCase ):
+    def setUp( self ):
+        self.workdir = tempfile.mkdtemp( 
+            prefix='listener-', suffix='-test', dir='/dev/shm' 
+        )
+        self.context = context.Context('default')
+        self.tokenizer = tokenizer.Tokenizer( self.context.dictionary_cache )
+    def tearDown( self ):
+        shutil.rmtree( self.workdir, True ) # ignore errors
+    def test_tokenizer_categories( self ):
+        for source,expected in [
+            ('ThisIsThat',['T','his','I','s','T','hat']),
+            ('this 23skeedoo',[u'this', u' ', u'23', u'skeedoo']),
+            ('Just so.',[u'J', u'ust', u' ', u'so', u'.']),
+            ('x != this',[u'x', u' ', u'!=', u' ', u'this']),
+            ('x == this',[u'x', u' ', u'==', u' ', u'this']),
+            (
+                'http://test.this.that/there?hello&ex#anchor',
+                [
+                    u'http', u'://', u'test', u'.', u'this', u'.', u'that', 
+                    u'/', u'there', u'?', u'hello', u'&', u'ex', u'#', u'anchor'
+                ]
+            ),
+            ('# What he said',[u'#', u' ', u'W', u'hat', u' ', u'he', u' ', u'said']),
+        ]:
+            raw_result = list(self.tokenizer.runs_of_categories(source))
+            result = [x[1] for x in raw_result]
+            assert result == expected, (source,result,raw_result)
+
+    def test_tokenizer_tokens( self ):
+        for source,expected in [
+            ('ThisIsThat',[['T','his','I','s','T','hat']]),
+            ('This is that',[[u'T', u'his'], [u' '], [u'is'], [u' '], [u'that']]),
+            ('x != this',[[u'x'], [u' '], [u'!='], [u' '], [u'this']]),
+            ('0x3faD',[['0','x','3','fa','D']]),
+            ('!@#$%^&*()_+-=[]{}\\|:;\'",.<>/?',[[u'!@#$%^&*()'], [u'_'], [u'+-=[]{}\\|:;\'",.<>/?']]),
+            ('elif moo:\n\tthat()',[[u'elif'], [u' '], [u'moo'], [u':'], [u'\n\t'], [u'that'], [u'()']]),
+        ]:
+            raw_result = list(self.tokenizer.runs_of_tokens( 
+                self.tokenizer.runs_of_categories(source)
+            ))
+            result = [[x[1] for x in result] for result in raw_result]
+            assert result == expected, (source,result,raw_result)
+
+    def test_expand( self ):
+        for source,expected in [
+            #('0x23ad',['zero','x','two','three','ad']), # word "Ad" as in advertisement
+            ('!==',['!=not-equal', '=equals']),
+            ('"',['"double-quote']),
+            ('ThisIsThat',['cap', 'camel', u'this', u'is', u'that']),
+            ('23skido',['two', 'three', 'no-space', u'skid', u'o', 'spaces']),
+        ]:
+            raw_result = list(self.tokenizer.runs_of_tokens( 
+                self.tokenizer.runs_of_categories(source)
+            ))
+            expanded = list(self.tokenizer.expand( raw_result ))[0]
+            assert expanded == expected, (source,expanded)
+
+    def test_break_down_name(self):
+        for input,expected in [
+            ('thisTest',['camel', 'this', 'test']),
+            ('ThisTest',['cap','camel', 'this', 'test']),
+            ('that_test',['that','_under-score','test']),
+            #('oneshot',['no-space','one','shot','spaces']), # would need statistical model
+        ]:
+            result = self.tokenizer.parse_camel( input )
+            assert result == expected, (input,result)
+    def test_run_together( self ):
+        dictionary = self.context.dictionary_cache
+        dictionary.add_dictionary_iterable([
+            ('kde','K D IY'),
+            ('veridian','MOO'),
+        ])
+        for run_together, expected in [
+            ('om',['o','m']),
+            ('buildthis',['build','this']),
+            ('Moveoverage',['move','over','age']),
+            ('generateov',['generate','o','v']),
+            ('qapplication',['q','application']),
+            ('kdebuildingwindow',['kde','building','window']),
+            ('VeridianEgg',['veridian','egg']),
+            ('Veridian',['veridian']),
+            #('oneshot',['one','shot']), # would need statistical model
+        ]:
+            result = self.tokenizer.parse_run_together( run_together )
+            assert result== expected, (run_together,result)
+        
