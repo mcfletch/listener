f76ab3c3c8e5aae55c0096a8ae61c71f36acadf8
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-09-04 16:46:30 -0400

    Playing around looking for a good pre-coding operation
    It *seems* like you could use the open silk encoder as the
    feed-stock for a recognizer, as it reduces down to a small
    number of integers for a given speech input.

--- listener/appsinkspike.py
+++ listener/appsinkspike.py
@@ -21,39 +21,46 @@ def main(filename=os.path.join( HERE, '../tests/fixtures/hello_world.wav' )):
         'audioconvert', '!',
         'audioresample', '!',
         'audio/x-raw-int,width=16,depth=16,channels=1,rate=8000', '!',
+        'opusenc',
+            'audio=false',
+            'bitrate=4000',
+            'bandwidth=1101',
+            '!',
+        'filesink',
+            'location=test.opusraw'
         #'autoaudiosink',
-        'appsink',
-            'name=app',
-            'enable-last-buffer=true',
-            'emit-signals=true',
-            'sync=true',
+#        'appsink',
+#            'name=app',
+#            'enable-last-buffer=true',
+#            'emit-signals=true',
+#            'sync=true',
     ]
     command = " ".join( pipeline_command )
     pipeline = gst.parse_launch(command)
-    app = pipeline.get_by_name( 'app' )
-    def on_new_buffer( appsink ):
-        # TODO: why doesn't on_new_buffer give us the 
-        # gst.Buf object? here we're using the last-buffer, 
-        # but that's not thread-safe
-        buf = appsink.get_property('last-buffer')
-        buf = numpy.frombuffer( buf.data, numpy.int16 )
-        # Example of doing something on the data, though 
-        # in a real app you'd combine the data on longer 
-        # time-scales such that you would see patterns 
-        # at the phoneme scale I suppose
-        raw = fft( buf )
-        frequencies = raw[:int(len(raw)/2)]
-        mags = numpy.round(numpy.absolute( frequencies ),0).astype('i')
-        max_bucket = (len(raw)/2-1)/(len(raw)/8000.)
-        
-        freqs = fftfreq( len(buf), d=(1/8000.))[:len(mags)]
-        mapped = zip([int(x) for x in mags],[int(y) for y in freqs])
-        mapped = sorted(mapped)
-        mapped = mapped[-10:]
-        for mag,freq in mapped:
-            print '%s - %s'%(freq, mag)
-        print
-    app.connect('new-buffer',on_new_buffer )
+#    app = pipeline.get_by_name( 'app' )
+#    def on_new_buffer( appsink ):
+#        # TODO: why doesn't on_new_buffer give us the 
+#        # gst.Buf object? here we're using the last-buffer, 
+#        # but that's not thread-safe
+#        buf = appsink.get_property('last-buffer')
+#        buf = numpy.frombuffer( buf.data, numpy.int16 )
+#        # Example of doing something on the data, though 
+#        # in a real app you'd combine the data on longer 
+#        # time-scales such that you would see patterns 
+#        # at the phoneme scale I suppose
+#        raw = fft( buf )
+#        frequencies = raw[:int(len(raw)/2)]
+#        mags = numpy.round(numpy.absolute( frequencies ),0).astype('i')
+#        max_bucket = (len(raw)/2-1)/(len(raw)/8000.)
+#        
+#        freqs = fftfreq( len(buf), d=(1/8000.))[:len(mags)]
+#        mapped = zip([int(x) for x in mags],[int(y) for y in freqs])
+#        mapped = sorted(mapped)
+#        mapped = mapped[-10:]
+#        for mag,freq in mapped:
+#            print '%s - %s'%(freq, mag)
+#        print
+#    app.connect('new-buffer',on_new_buffer )
 
     bus = pipeline.get_bus()
     bus.add_signal_watch()
